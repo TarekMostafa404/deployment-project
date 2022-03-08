c0b3d98578ed614c32a8f46eee7f23aec143d041
Tarek Mostafa <80650764+TarekMostafa404@users.noreply.github.com>
2022-03-08 17:11:05 +0200

    mv env

--- udagram-frontend/bin/deploy.sh
+++ udagram-frontend/bin/deploy.sh
@@ -1,1 +1,7 @@
 aws s3 cp --recursive --acl public-read ./www s3://fullstack-udagram/
+eb setenv DB_PORT=$DB_PORT
+eb setenv POSTGRES_DB=$POSTGRES_DB
+eb setenv POSTGRES_HOST	=$POSTGRES_HOST	
+eb setenv POSTGRES_PASSWORD	=$POSTGRES_PASSWORD	
+eb setenv POSTGRES_USERNAME=$POSTGRES_USERNAME
+eb setenv URL=$URL
